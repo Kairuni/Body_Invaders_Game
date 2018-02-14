230d19763044900ac2bdacf07bef8eddd26ebd42
Keegan <keegan@kairuni.com>
2018-02-14 00:06:41 -0800

    This is gonna wreck tracking, but weirdness was happening. It'll be FINEEEEE

--- Wall.js
+++
@@ -1,8 +0,0 @@
-class Wall extends Entity {
-    constructor(game, pos, dim, img) {
-        super(game, pos, dim);
-
-        this.game.addStaticEntity(this);
-        this.game.partitioner.addToGrid(this, 0);
-    }
-}
